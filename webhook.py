c28dc38e6d5ef39bddc915db0f383584a51e0427
nialvape <joacopenialva@gmail.com>
2025-10-31 14:43:27 -0300

    al pedo

--- webhook.py
+++ webhook.py
@@ -9,12 +9,16 @@ phone = "5491150128981"
 session: dict[str, State] = {}
 while user_input != "EOF":
     flags = user_input.split(" ")
-    if flags[0] == "-n":
-        if flags[1].isdigit():
-            phone = flags[1]
-            print(f"phone number changed to {phone}")
-            continue
+    if flags[0] == "-n" and flags[1].isdigit():
+        phone = flags[1]
+        print(f"phone number changed to {phone}")
+        #fake webhook listening
+        user_input = input("Write something: ")
+        continue
+    if flags[0] == "-n" and !(flags[1].isdigit()):
         print("just digits are allowed for -n")
+        #fake webhook listening
+        user_input = input("Write something: ")
         continue
 
     state = session.get(phone, {
